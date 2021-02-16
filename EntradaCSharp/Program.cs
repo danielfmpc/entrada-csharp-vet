489522d5a3dba1a8232fc94fcd8e869569527723
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-15 22:53:23 -0300

    Criando split e mostrando resultado

--- EntradaCSharp/Program.cs
+++ EntradaCSharp/Program.cs
@@ -6,7 +6,15 @@ namespace EntradaCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite as cores");
+            string x = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(x);
+            Console.WriteLine(v[0]);
+            Console.WriteLine(v[1]);
+            Console.WriteLine(v[2]);
         }
     }
 }
